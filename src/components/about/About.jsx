e93349036721cc775de886a51c283b91bbd0cb6f
Lyssa <ebonwolf00@gmail.com>
2022-05-04 08:53:56 +0100

    all images added to the assets folder, reusable variables made for images that are used more than once in the _variables.scss file

---
+++ src/components/about/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../../containers/navbar/NavBar';
+
+const About = () => {
+  return (
+    <div className='about-page'>
+      <NavBar />
+    </div>
+  );
+};
+
+export default About;
